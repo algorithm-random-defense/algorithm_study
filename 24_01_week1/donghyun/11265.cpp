afdcd33e5f3f6223420d292893a83d71fadd17c9
김동현 <donghyun@gimdonghyeon-ui-MacBookPro.local>
2024-01-03 21:27:58 +0900

    플로이드 와샬첨보보

--- 24_01_week1/donghyun/11265.cpp
+++ 24_01_week1/donghyun/11265.cpp
@@ -1,8 +1,36 @@
-//
+// 끝나지 않는 파티
 #include <iostream>
-
+#include <vector>
 using namespace std;
+vector<vector<int> > board;
 
 int main() {
 	ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+	int n, m;
+	cin >> n >> m;
+	board = vector<vector<int> >(n,vector<int>(n));
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++)
+			cin >> board[i][j];
+	}
+
+	for (int k = 0; k < n; k++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (board[i][k] + board[k][j] < board[i][j])
+					board[i][j] = board[i][k] + board[k][j];
+			}
+		}
+	}
+
+	while (m--) {
+		int a, b, time;
+		cin >> a >> b >> time;
+		if (board[a - 1][b - 1] <= time)
+			cout << "Enjoy other party\n";
+		else
+			cout << "Stay here\n";
+	}
 }
